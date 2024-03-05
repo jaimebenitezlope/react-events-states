1306fdd48a4d59e2007f12737c3ae7077f5701cd
jaimebenitezlope <jaimebenitezlope@icloud.com>
2024-03-05 09:30:33 +0100

    9:30

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+import { USERS } from '../../constants/users';
+
 const Users = () => {
+	const [userCounter, setUserCounter] = useState(0);
+	const currentUser = USERS[userCounter];
+	const isFirstUser = userCounter === 0;
+	const isLastUser = userCounter === USERS.length - 1;
+
 	return (
 		<>
 			<h1>Slide Users</h1>
+			<img src={currentUser.profileImage} alt='' />
+			<h2>{currentUser.name}</h2>
+			<p>{currentUser.username}</p>
+			<p>{currentUser.email}</p>
+			<button
+				disabled={isFirstUser}
+				onClick={() => setUserCounter(userCounter - 1)}
+			>
+				Previus
+			</button>
+			<button
+				disabled={isLastUser}
+				onClick={() => setUserCounter(userCounter + 1)}
+			>
+				Next
+			</button>
 		</>
 	);
 };
